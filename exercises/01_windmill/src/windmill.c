931517dee568df564a360b39627628e574ab9cb0
J Carlos Viana F <jcvf@ic.ufal.br>
2018-02-05 00:32:55 -0300

    project refactored

--- exercises/01_windmill/src/windmill.c
+++ exercises/01_windmill/src/windmill.c
@@ -7,6 +7,13 @@
 const double PI = 3.141592654;
 int direction = 1;
 int degree = 0;
+const float colors[4][3] =
+    {
+        {0.0f,1.0f,0.0f},
+        {1.0f,0.0f,0.0f},
+        {0.0f,0.0f,1.0f},
+        {1.0f,1.0f,0.0f}
+    };
 
 void SpecialInput(int key, int x, int y)
 {
@@ -28,23 +35,31 @@ float get_rad(float mod, float mult) {
  
 void windmill() {
     int i;
+    float center_x = 0.0f;
+    float center_y = 0.2f;
+    float adjust_radius = 0.2f;
+    float adjust_spade = 25;
+
     glColor3f(1.0f, 1.0f, 1.0f);
+    
     glBegin(GL_TRIANGLES);
     
-    glVertex2f(0, 0);
-    glVertex2f(0.05f, 3);
-    glVertex2f(-0.05f, 3);
+    glVertex2f(0, -0.8f);
+    glVertex2f(0.005f, 0.2f);
+    glVertex2f(-0.005f, 0.2f);
     
     glEnd();
     
     for (i = 0; i < 4; i++) {
-        glColor3f(0.1f * i, 0.05f + i * 0.2f, 0.3f * i);
+        glColor3f(colors[i][0],colors[i][1],colors[i][2]);
 
         glBegin(GL_TRIANGLES);
         
-        glVertex2f(0.0f,3.0f);
-        glVertex2f(sin(get_rad(0,i)), cos(get_rad(0,i))+3.0f);
-        glVertex2f(sin(get_rad(25,i)), cos(get_rad(25,i))+3.0f);
+        glVertex2f(center_x, center_y);
+        glVertex2f((sin(get_rad(0,i))*adjust_radius) + center_x,
+            (cos(get_rad(0,i))*adjust_radius) + center_y);
+        glVertex2f((sin(get_rad(adjust_spade,i))*adjust_radius) + center_x,
+            (cos(get_rad(adjust_spade,i))*adjust_radius) + center_y);
         
         glEnd();
     }
@@ -53,14 +68,9 @@ void windmill() {
 void windmill_display() {
  
     glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
- 
-    glPushMatrix();
-    glTranslated(3.5,-0.7,1);
-    glScaled(1.0,1.0,1);
+
     glutSpecialFunc(SpecialInput);
     windmill();
-    glPopMatrix();
  
     glutSwapBuffers();
  
@@ -74,8 +84,4 @@ void windmill_doFrame(int v) {
  
 void windmill_init() {
     glClearColor(1.0f, 0.6f, 0, 1);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, 7, -1, 4, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
 }
--- exercises/01_windmill/src/windmill.h
+++ exercises/01_windmill/src/windmill.h
@@ -1,10 +1,19 @@
 #ifndef WINDMILL_H
 #define WINDMILL_H
 
+/**
+ * draw
+ */
 void windmill_display();
 
+/**
+ * update
+ */
 void windmill_doFrame(int v);
  
+/**
+ * init
+ */
 void windmill_init();
 
 #endif // WINDMILL_H
